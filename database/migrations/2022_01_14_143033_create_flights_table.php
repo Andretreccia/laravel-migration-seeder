f4140827a744eaccea4b4718b88b8d69a01c5d55
Andretreccia <andreabtreccia@gmail.com>
2022-01-14 15:55:09 +0100

    Create flights tables with artisan migrate methods

---
+++ database/migrations/2022_01_14_143033_create_flights_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            //
+            $table->id();
+            $table->string('package-name');
+            $table->string('travel-destination');
+            $table->text('description');
+            $table->string('duration');
+            $table->float('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('flights', function (Blueprint $table) {
+            //
+        });
+    }
+}
